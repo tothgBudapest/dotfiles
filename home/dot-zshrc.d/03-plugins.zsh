90008a30da2db98d5f1a51d2c0235a94f04296b7
Balint Antal <balint@denv.io>
2025-02-20 21:53:40 +0100

    fix(*): substring searching with autocompletion + indentation

--- home/dot-zshrc.d/03-plugins.zsh
+++ home/dot-zshrc.d/03-plugins.zsh
@@ -1,4 +1,8 @@
 #!/bin/zsh
 znap source ael-code/zsh-colored-man-pages
-znap source zsh-users/zsh-autosuggestions
 znap source zsh-users/zsh-syntax-highlighting
+
+znap source zsh-users/zsh-autosuggestions
+znap source zsh-users/zsh-history-substring-search
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
