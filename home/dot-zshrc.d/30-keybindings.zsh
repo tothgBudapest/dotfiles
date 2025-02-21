702c31f1e5407ce87210ea673aac021b553b3a88
Balint Antal <balint@denv.io>
2025-02-21 16:49:25 +0100

    chore(zsh): add more macOS native keybinds

--- home/dot-zshrc.d/30-keybindings.zsh
+++ home/dot-zshrc.d/30-keybindings.zsh
@@ -1,5 +1,14 @@
 #!/bin.zsh
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
-bindkey "^[b" backward-word # Fix word-jumping (⌥←)
-bindkey "^[f" forward-word  # Fix word-jumping (⌥→)
+
+# Native macOS Behaviour #
+# ---------------------- #
+# Use Emacs keybindings
+bindkey -e
+# ⌥ + Left / Right (Jump by word)
+bindkey "^[b" backward-word   # ⌥ + ←
+bindkey "^[f" forward-word    # ⌥ + →
+# ⌘ + Left / Right (Move to start/end of line)
+bindkey "^A" beginning-of-line  # ⌘ + ←
+bindkey "^E" end-of-line        # ⌘ + →
