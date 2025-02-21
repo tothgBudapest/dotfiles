4912cebb25b5b9ab0a4e27eb1243bfa3f79d675b
Balint Antal <balint@denv.io>
2025-02-21 16:31:20 +0100

    chore(zsh): fix word-jumping

--- home/dot-zshrc.d/25-plugins.zsh
+++ home/dot-zshrc.d/25-plugins.zsh
@@ -1,8 +1,13 @@
 #!/bin/zsh
 znap source ael-code/zsh-colored-man-pages
-znap source zsh-users/zsh-syntax-highlighting
-
 znap source zsh-users/zsh-autosuggestions
 znap source zsh-users/zsh-history-substring-search
+znap source zsh-users/zsh-syntax-highlighting
+
+# Fix compatibility with history-substring-search
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
+
+# Fix word-jumping (⌥←/⌥→)
+bindkey "^[b" backward-word
+bindkey "^[f" forward-word
