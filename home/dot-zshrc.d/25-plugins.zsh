47d1184dc9c6c443f06a75ecadbb5a06ac401fa3
Balint Antal <balint@denv.io>
2025-02-24 11:38:17 +0100

    fix(zsh): reorder zsh-history-substring-search

--- home/dot-zshrc.d/25-plugins.zsh
+++ home/dot-zshrc.d/25-plugins.zsh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 znap source ael-code/zsh-colored-man-pages
 znap source zsh-users/zsh-autosuggestions
-znap source zsh-users/zsh-history-substring-search
+
+# Must be loaded before zsh-history-substring-search
 znap source zsh-users/zsh-syntax-highlighting
+znap source zsh-users/zsh-history-substring-search
--- home/dot-zshrc.d/30-keybindings.zsh
+++ home/dot-zshrc.d/30-keybindings.zsh
@@ -1,14 +1,16 @@
 #!/bin.zsh
-bindkey '^[[A' history-substring-search-up
-bindkey '^[[B' history-substring-search-down
 
-# Native macOS Behaviour #
-# ---------------------- #
 # Use Emacs keybindings
 bindkey -e
+
 # ⌥ + Left / Right (Jump by word)
 bindkey "^[b" backward-word   # ⌥ + ←
 bindkey "^[f" forward-word    # ⌥ + →
+
 # ⌘ + Left / Right (Move to start/end of line)
 bindkey "^A" beginning-of-line  # ⌘ + ←
 bindkey "^E" end-of-line        # ⌘ + →
+
+# Search history with arrow keys
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
